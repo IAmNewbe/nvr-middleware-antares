5fe98d49a5782b0f1b3ac64d8abc1d8bbae9b82a
IAmNewbe <ahlulhikam2222@gmail.com>
2024-09-30 14:44:26 +0700

    init

---
+++ src/widgets/layout/sidenav.jsx
@@ -0,0 +1,114 @@
+import PropTypes from "prop-types";
+import { Link, NavLink } from "react-router-dom";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import {
+  Avatar,
+  Button,
+  IconButton,
+  Typography,
+} from "@material-tailwind/react";
+import { useMaterialTailwindController, setOpenSidenav } from "@/context";
+import antaresLogo from "/img/logo/logo_antares.svg"
+
+export function Sidenav({ brandImg, brandName, routes }) {
+  const [controller, dispatch] = useMaterialTailwindController();
+  const { sidenavColor, sidenavType, openSidenav } = controller;
+  const sidenavTypes = {
+    dark: "bg-gradient-to-br from-gray-800 to-gray-900",
+    white: "bg-white shadow-sm",
+    transparent: "bg-transparent",
+  };
+
+  return (
+    <aside
+      className={`${sidenavTypes[sidenavType]} ${
+        openSidenav ? "translate-x-0" : "-translate-x-80"
+      } fixed inset-0 z-50 h-full w-72 rounded-none transition-transform duration-300 xl:translate-x-0 border `}
+    >
+      <div
+        className={`relative`}
+      >
+        <Link to="/" className="py-6 px-8 text-center justify-center">
+          <img src={antaresLogo} alt="alternative" className="mx-auto my-2 md:my-5"/>
+          <Typography
+            variant="h6"
+            color={"dark"}
+          >
+            {brandName}
+          </Typography>
+        </Link>
+        <IconButton
+          variant="text"
+          color="white"
+          size="sm"
+          ripple={false}
+          className="absolute right-0 top-0 grid rounded-br-none rounded-tl-none xl:hidden"
+          onClick={() => setOpenSidenav(dispatch, false)}
+        >
+          <XMarkIcon strokeWidth={2.5} className="h-5 w-5 text-white" />
+        </IconButton>
+      </div>
+      <div className="m-4">
+        {routes.map(({ layout, title, pages }, key) => (
+          <ul key={key} className="mb-4 flex flex-col gap-1">
+            {title && (
+              <li className="mx-3.5 mt-4 mb-2">
+                <Typography
+                  variant="small"
+                  color={sidenavType === "dark" ? "white" : "blue-gray"}
+                  className="font-black uppercase opacity-75"
+                >
+                  {title}
+                </Typography>
+              </li>
+            )}
+            {pages.map(({ icon, name, path }) => (
+              <li key={name}>
+                <NavLink to={`/${layout}${path}`}>
+                  {({ isActive }) => (
+                    <Button
+                      variant={isActive ? "bg-orange-700" : "text"}
+                      color={
+                        isActive
+                          ? sidenavColor
+                          : sidenavType === "orange"
+                          ? "white"
+                          : "bg-orange-400"
+                      }
+                     
+                      className="flex items-center gap-4 px-4 capitalize"
+                      fullWidth
+                    >
+                      {icon}
+                      <Typography
+                        color="inherit"
+                        className="font-medium capitalize"
+                      >
+                        {name}
+                      </Typography>
+                    </Button>
+                  )}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        ))}
+      </div>
+    </aside>
+  );
+}
+
+Sidenav.defaultProps = {
+  brandImg: "/img/logo-ct.png",
+  brandName: "Antares Cam Snapshot",
+};
+
+Sidenav.propTypes = {
+  brandImg: PropTypes.string,
+  brandName: PropTypes.string,
+  routes: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+Sidenav.displayName = "/src/widgets/layout/sidnave.jsx";
+
+export default Sidenav;
